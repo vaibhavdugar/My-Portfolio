99d26839ef0dfaf28c4e561b2c9619399c8245de
Vaibhav <vrdugar02@gmail.com>
2023-05-06 12:22:03 +0530

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // Used for customization
 module.exports = {
+  distDir: "out",
   content: ["./*.html"], // using this in content array means in all the HTML files where utility classes are being used, tailwind css will generate styles
   theme: {
     screens: {
