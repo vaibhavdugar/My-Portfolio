87dec6594a60b50f8054a45ecf137e4ad0a4090c
Vaibhav <vrdugar02@gmail.com>
2023-05-06 11:30:17 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+// Used for customization
+module.exports = {
+  content: ["./*.html"], // using this in content array means in all the HTML files where utility classes are being used, tailwind css will generate styles
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "767px",
+      lg: "1069px",
+      xl: "1408px",
+      "2xl": "1440px",
+    },
+    extend: {
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(to right, #ff0000, #0000ff)",
+      },
+    },
+    fontFamily: {
+      rakkas: ["Rakkas", "cursive"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
